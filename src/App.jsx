f2bc030188f3843ddb04e6294ee4f8b4e96b3d89
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-28 13:45:19 +0400

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import Form from "./Form";
+import Items from "./Items";
+
 const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
+  const [items, setItems] = useState([]);
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+
+    setItems([...items, newItem]);
+  };
+
+  const removeItem = (itemId) => {
+    const newItem = items.filter((item) => item.id !== itemId);
+    setItems(newItem);
+  };
+
+  return (
+    <section className="section-center">
+      <Form addItem={addItem} />
+      <Items items={items} removeItem={removeItem} />
+    </section>
+  );
 };
 
 export default App;
