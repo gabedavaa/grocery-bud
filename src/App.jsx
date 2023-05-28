6494885f0725ba0d10ba67d2c35e0066a8026edd
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-28 15:01:28 +0400

    adding local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import { nanoid } from "nanoid";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import Form from "./Form";
 import Items from "./Items";
 
+// const getLocalStorage = () => {
+//   let list = localStorage.getItem("list");
+
+//   if (list) {
+//     list = JSON.parse(localStorage.getItem("list"));
+//   } else {
+//     list = [];
+//   }
+
+//   return list;
+// };
+
+const setLocalStorage = (items) => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+
+const defaultList = JSON.parse(localStorage.getItem("list" || "[]"));
+
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
@@ -12,19 +31,38 @@ const App = () => {
       completed: false,
       id: nanoid(),
     };
-
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success("item added to the list");
   };
 
   const removeItem = (itemId) => {
-    const newItem = items.filter((item) => item.id !== itemId);
-    setItems(newItem);
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success("item removed from the list");
+  };
+
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success("item done");
   };
 
   return (
     <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form addItem={addItem} />
-      <Items items={items} removeItem={removeItem} />
+      <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
   );
 };
